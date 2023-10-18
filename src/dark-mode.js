98d62725bcd27f02c6cecd02abfbefb5903c7290
ellen.jarvenpaa <ellen.jarvenpaa@metropolia.fi>
2023-10-18 14:13:50 +0300

    valmis

--- src/dark-mode.js
+++
@@ -1,7 +0,0 @@
-const checkbox = document.getElementById("checkbox");
-checkbox.addEventListener("change", () => {
-  document.body.classList.toggle("dark");
-
-  let resarea = document.querySelector(".grid");
-  resarea.classList.toggle("dark");
-});
